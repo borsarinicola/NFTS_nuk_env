a6c9ea49b0659e01f9b9aecc16dc9a4129f99750
borsarinicola <58230671+borsarinicola@users.noreply.github.com>
2019-12-08 00:12:09 +0000

    code optimisation

--- python/nLabelShortcut.py
+++ python/nLabelShortcut.py
@@ -1,42 +1,26 @@
 ####################################
-#Nicola Borsari nLabelShortcut - 17 August 2019 - v3.0
+#Nicola Borsari nLabelShortcut - 8 December 2019 - v3.5
 
 import nuke
-import os
-import os.path
-import nukescripts
+
 
 def nLabelShortcut():
 
     selection = nuke.selectedNodes() #store selection into list
-    x = 0 #counter for loop
 
-    for each in selection:
-        processing = selection[x]
+    for x in selection:
 
-        if x <= len(selection):
+        #storing selected node name into variable
+        selectionName = x.knob('name').getValue()
 
-            x = x+1
+        #storing node existing lable into variable
+        oldLabel = x.knob('label').getValue()
 
-            #clear selection and prepare a new one for processing 
-            nukescripts.clear_selection_recursive()
-            processing.knob("selected").setValue(True)
+        #asking for new label while autofilling with the existing one
+        INlabel = nuke.getInput("New Label for " + selectionName, oldLabel)
 
-            #storing selected node name into variable
-            selectionName = nuke.selectedNode().knob('name').getValue()
-
-            #storing node existing lable into variable
-            oldLabel = nuke.selectedNode().knob('label').getValue()
-
-            #asking for new label while autofilling with the existing one
-            INlabel = nuke.getInput("New Label for " + selectionName, oldLabel)
-
-            #set 
-            nuke.selectedNode().knob('label').setValue(INlabel)          
-    
-        else:
-
-            break    
+        #set 
+        x.knob('label').setValue(INlabel)          
 
     return
 ####################################
