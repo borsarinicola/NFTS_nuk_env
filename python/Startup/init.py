64724607767c50ebff023598b83bfc9b0d0e03c8
borsarinicola <58230671+borsarinicola@users.noreply.github.com>
2021-11-23 17:21:21 +0000

    make project code token global

--- python/Startup/init.py
+++ python/Startup/init.py
@@ -18,7 +18,18 @@ def shot_addUserResolveEntries(self, resolver):
     def plateRez(task):
         rez = '{}x{}'.format(plateWidth(task), plateHeight(task))
         return rez
+      
 
+    resolver.addResolver("{width}", "Returns the width of the source plate", lambda keyword, task: plateWidth(task))
+    resolver.addResolver("{height}", "Returns the height of the source plate", lambda keyword, task: plateHeight(task))
+    resolver.addResolver("{rez}", "Returns the height Resoluton of the source palte (width x height)", lambda keyword, task: plateRez(task))
+
+ShotProcessorPreset.addUserResolveEntries = shot_addUserResolveEntries
+
+
+
+def global_addUserResolveEntries(self, resolver):
+    
     def generate_project_code(task, project_name):
         temp_string = ''
         if len(project_name) <= 3:
@@ -30,12 +41,8 @@ def shot_addUserResolveEntries(self, resolver):
         
             if len(temp_string) == 3:
                 return temp_string
-        
 
-
-    resolver.addResolver("{width}", "Returns the width of the source plate", lambda keyword, task: plateWidth(task))
-    resolver.addResolver("{height}", "Returns the height of the source plate", lambda keyword, task: plateHeight(task))
-    resolver.addResolver("{rez}", "Returns the height Resoluton of the source palte (width x height)", lambda keyword, task: plateRez(task))
     resolver.addResolver("{project_code}", "Three letter project code auto-generated using the hiero project name", lambda keyword, task: generate_project_code(task, hiero.core.projects()[-1].name()))
 
-ShotProcessorPreset.addUserResolveEntries = shot_addUserResolveEntries
+# This token can be applied to ANY export so add it to the base class
+hiero.core.TaskPresetBase.addUserResolveEntries = global_addUserResolveEntries
